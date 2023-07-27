5f75046a67889e87915f0d8420d5e40f77fd4f34
sangbum choi <sangbumchoi@sangbumui-MacBookPro.local>
2023-07-27 19:52:46 +0900

    Initial Commit

---
+++ abuse-detector/abuse_detectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  abuse_detectorApp.swift
+//  abuse-detector
+//
+//  Created by sangbum choi on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct abuse_detectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
